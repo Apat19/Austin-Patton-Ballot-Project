250da3c0005bd80b83b6a7b44fb9d3f8b4ce949c
Austin Patton <SCS374Group1@gmail.com>
2023-04-29 12:21:36 -0400

    assignment complete

---
+++ Austin Patton Ballot Project/TBViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TBViewController.swift
+//  Austin Patton Ballot Project
+//
+//  Created by Austin Patton on 4/29/23.
+//
+
+import UIKit
+
+class TBViewController: UITabBarController {
+    var blueVote: Int = 0
+    var redVote: Int = 0
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+}
--- Austin Patton Ballot Project/ViewController.swift
+++ Austin Patton Ballot Project/ViewController.swift
@@ -9,8 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+  
+    @IBOutlet weak var redVoteCounter: UILabel!
+    @IBOutlet weak var blueVoteCounter: UILabel!
+    @IBOutlet weak var winnerDeclare: UILabel!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        blueVoteCounter.text = String((parent as! TBViewController).blueVote)
+        redVoteCounter.text = String((parent as! TBViewController).redVote)
+        
+      
+        
+        if (parent as! TBViewController).blueVote > (parent as! TBViewController).redVote {
+            winnerDeclare.text = "Blue!"
+        } else {
+            winnerDeclare.text = "Red"
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
     }
 
